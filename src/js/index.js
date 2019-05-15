4875a13aae61ce47d3c7f21eac4ecdd0b6760e3b
Martinrh83 <martinrh83@gmail.com>
2019-05-15 01:47:35 -0300

    Updated servings

--- src/js/index.js
+++ src/js/index.js
@@ -75,3 +75,16 @@ const controlRecipe = async () => {
 
 //window.addEventListener('hashchange', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+//Handling recipe button clicks
+elements.recipe.addEventListener('click', e =>{
+  if(e.target.matches('.btn-decrease, .btn-decrease *')){
+    if(state.recipe.servings > 1){
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }else if (e.target.matches('.btn-increase, .btn-increase *')){
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+});
