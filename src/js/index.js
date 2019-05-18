984c37b717fc76d329b94a3bccdd774aaeb23ed4
Martinrh83 <martinrh83@gmail.com>
2019-05-17 23:56:19 -0300

    Added controlLike in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from  './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView'
 import {elements, renderLoader, clearLoader} from './views/base';
+
 
 /*Global state of the app*/ 
 const state = {};
@@ -82,7 +84,7 @@ const controlRecipe = async () => {
 //list controller
 const controlList = () => {
   //create a new list if there is none yet
-  if (!state.list) state.list = new List();
+  if (!state.list) state.list = new Likes();
 
   // add each ingredient to the list and UI
   state.recipe.ingredients.forEach( el => {
@@ -91,6 +93,22 @@ const controlList = () => {
   });
 }
 
+const controlLike = () => {
+  if(!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+  if(!state.likes.isLiked(currentID)){
+    //add like to the state
+    const newLike = state.likes.addLike(currentID, state.recipe.title, state.recipe.author, state.recipe.img);
+    //toggle the like button
+
+    //add like to UIlist
+    console.log(state.likes);
+  }else{
+    //remove like from the state
+    state.likes.deleteLike(currentID);
+    //remove like from UI list
+  }
+}
 //handle delete and update list item
 elements.shopping.addEventListener('click', e => {
   const id = e.target.closest('.shopping__item').dataset.itemid;
@@ -119,7 +137,9 @@ elements.recipe.addEventListener('click', e =>{
     recipeView.updateServingsIngredients(state.recipe);
   }else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
     controlList();
-  } 
+  }else if(e.target.matches('.recipe__love, .recipe__love *')){
+    controlLike();
+  }
 });
 
 const l = new List();
